6d6fe168fd6fee217c715fbec8ee123ab869fda4
paulbasaur <pauldackeng@gmail.com>
2022-10-04 23:03:26 -0400

    Add player choice, play round, and game

--- javascript.js
+++ javascript.js
@@ -1,9 +1,50 @@
-let choices = ["rock", "paper", "scissors"];
-
+const choices = ["rock", "paper", "scissors"];
+let score = 0;
 
 function getComputerChoice() {
-  randChoiceIndex = Math.floor(Math.random() * 3);
+  let randChoiceIndex = Math.floor(Math.random() * 3);
   return choices[randChoiceIndex];
 }
 
-console.log(getComputerChoice());
+
+function getPlayerChoice() {
+  let playerChoice = prompt("Rock? Paper? Scissors?").toLowerCase();
+  while (!(choices.includes(playerChoice))) {
+    playerChoice = prompt("Please input rock, paper, or scissors.").toLowerCase();
+  }
+  return playerChoice;
+}
+
+
+function playRound() {
+  const computerWinCombos = ["rockscissors", "scissorspaper", "paperrock"];
+  let computerChoice = getComputerChoice();
+  let playerChoice = getPlayerChoice();
+  while (computerChoice === playerChoice) {
+    computerChoice = getComputerChoice();
+    playerChoice = getPlayerChoice();
+  }
+  if (computerWinCombos.includes(computerChoice + playerChoice)) {
+    console.log("Computer wins round");
+    return (-1);
+  } else {
+    console.log("Player wins round");
+    return (1);
+  }
+}
+
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    console.log(score);
+    let round = playRound();
+    score += round;
+  }
+  if (score > 0) {
+    console.log("Player wins match!");
+  } else {
+    console.log("Computer wins match!")
+  }
+}
+
+game();
